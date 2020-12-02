a2442c1439975e9a085015c5c6b92965c501fbd6
周邓 <deng.zhou@hand-china.com>
2020-12-02 10:39:25 +0800

    add：创建一个最基本的React页面，并启动项目

---
+++ src/App.js
@@ -0,0 +1,7 @@
+
+import React from 'react';
+
+export default function App() {
+    return <h1>React页面 2020-12-01 dengdeng</h1>;
+}
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,40 @@
 
-// // demo1: 最基本的打包案例
-// const path = require('path');
-// module.exports = {
-//     // 入口文件
-//     // entry: './src/index-demo1.js', // 或者：
-//     entry: __dirname + '/src/index-demo1.js',
-//     // __dirname是node.js中的一个全局变量，它指向当前执行脚本所在的目录：当前文件在根目录下，所以指向值为根目录
-
-//     // 出口文件
-//     output: {
-//         filename: 'bundle-demo1.js',      // 打包后的文件名称
-//         // path: path.resolve('dist')  // 打包后的目录，必须是绝对路径
-//         // path: path.resolve(__dirname, 'dist')
-//         path: __dirname + '/dist',
-//     },
-// };
-
-
-// demo2: 
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/index-demo1.js',
-
+    mode: 'development',
+    entry: './src/index.js',
     output: {
-        filename: 'bundle-demo1.js',
-        path: path.resolve('dist')
+        filename: 'bundle.js',      // 打包后的文件名称
+        path: path.resolve('dist'),  // 打包后的目录，必须是绝对路径
     },
-
+    // 处理对应模块
+    module: {
+        rules: [
+            {
+                test: /\.(js|mjs|jsx)$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            '@babel/preset-env',
+                            '@babel/preset-react'
+                        ]
+                    }
+                }
+            }
+        ],
+    },
     plugins: [
-        new HtmlWebpackPlugin(), // 无参数版
-    ],
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        }),
+    ],             // 对应的插件
+    // 开发服务器配置
+    devServer: {
+        port: 8888, // 端口号 设置 默认8080
+        open: true, // 自动打开浏览器 默认不打开
+    },
 };
-
-// webpack打包时错误 Cannot find module 'webpack/lib/node/NodeTemplatePlugin'
-// 解决办法：npm link webpack --save-dev
-// 该命令的作用是将一个任意位置的npm包链接到全局执行环境，可以看到控制台输出了：
-// E:\等等学习\前端\webpackLearning\node_modules\webpack -> C:\Users\周邓\AppData\Roaming\npm\ild-compiler.js',node_modules\webpack
-// 之后再执行 webpack 打包，打包成功.
